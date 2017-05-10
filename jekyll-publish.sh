87f94c4c8c69b81740c6e301c56a519da2c1e2ed
Alex Scherbanov <alex@egotv.ru>
2017-05-10 19:21:38 +0300

    fix publishing

--- jekyll-publish.sh
+++ jekyll-publish.sh
@@ -17,12 +17,11 @@ docker run -it --rm --name=jekyll-publish \
 # the whole _site/ dir gets rewritten after jekyll build
 cp README-master-branch.md _site/README.md
 echo $(date) >> _site/commit
-echo $(git rev-parse --short HEAD) >> _site/commit
+echo $(git rev-parse --short --verify HEAD) >> _site/commit
 
 
 
 cd _site
 git add --all && \
-    git -c "user.name=jekyll-publish" -c "user.email=NA" commit --allow-empty-message -am '' && \
-    git push
+    git -c "user.name=jekyll-publish" -c "user.email=NA" commit --allow-empty-message -am ''      # && git push
 

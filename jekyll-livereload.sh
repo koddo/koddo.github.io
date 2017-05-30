7b229d6366dc024c073a7059dc05ca9e6eb0c652
Alex Scherbanov <alex@egotv.ru>
2017-05-30 16:42:30 +0300

    make livereload script to take port as a param

--- jekyll-livereload.sh
+++ jekyll-livereload.sh
@@ -4,9 +4,12 @@
 
 ## please make sure listening to 0.0.0.0 is ok in your environment
 
-docker run -it --rm --name=jekyll-livereload \
+NAME=jekyll_livereload_$(basename $(pwd))
+PORT=${1:-35729}
+
+docker run -it --rm --name="$NAME" \
     --volume=$(pwd):/srv/jekyll \
-    -p 0.0.0.0:35729:35729 \
+    -p 0.0.0.0:$PORT:35729 \
     koddo/jekyll \
     guard
 
